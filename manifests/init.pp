2515275e720d3d6e888c978cf8110e7b2adc67d5
Hunter Satterwhite <hsatterwhite@webassign.net>
2014-05-13 16:13:01 -0400

    Removed if conditional for snmptrapd.conf

--- manifests/init.pp
+++ manifests/init.pp
@@ -379,17 +379,15 @@ class snmp (
     }
   }
 
-  if $trap_service_ensure == 'running' {
-    file { 'snmptrapd.conf':
-      ensure  => $file_ensure,
-      mode    => $snmp::params::service_config_perms,
-      owner   => 'root',
-      group   => 'root',
-      path    => $snmp::params::trap_service_config,
-      content => template('snmp/snmptrapd.conf.erb'),
-      require => Package['snmpd'],
-      notify  => $snmptrapd_conf_notify,
-    }
+  file { 'snmptrapd.conf':
+    ensure  => $file_ensure,
+    mode    => $snmp::params::service_config_perms,
+    owner   => 'root',
+    group   => 'root',
+    path    => $snmp::params::trap_service_config,
+    content => template('snmp/snmptrapd.conf.erb'),
+    require => Package['snmpd'],
+    notify  => $snmptrapd_conf_notify,
   }
 
   if $::osfamily == 'RedHat' {
