e272704f357c3c70c162adf7b33e2b43d2c57315
Tim Meusel <tim@bastelfreak.de>
2018-09-14 22:07:53 +0200

    rubocop: autofix

--- spec/classes/snmp_client_spec.rb
+++ spec/classes/snmp_client_spec.rb
@@ -2,26 +2,27 @@
 
 require 'spec_helper'
 
-describe 'snmp::client', :type => 'class' do
-
+describe 'snmp::client', type: 'class' do
   context 'on a non-supported osfamily' do
-    let(:params) {{}}
-    let :facts do {
-      :osfamily        => 'foo',
-      :operatingsystem => 'bar'
-    }
-    end
-    it 'should fail' do
-      expect {
-        should raise_error(Puppet::Error, /Module snmp is not supported on bar/)
+    let(:params) { {} }
+    let :facts do
+      {
+        osfamily: 'foo',
+        operatingsystem: 'bar'
       }
+    end
+
+    it 'fails' do
+      expect do
+        is_expected.to raise_error(Puppet::Error, %r{Module snmp is not supported on bar})
+      end
     end
   end
 
   redhatish = ['RedHat']
-  #redhatish = ['RedHat', 'Fedora']
+  # redhatish = ['RedHat', 'Fedora']
   debianish = ['Debian']
-  #debianish = ['Debian', 'Ubuntu']
+  # debianish = ['Debian', 'Ubuntu']
   suseish = ['Suse']
   freebsdish = ['FreeBSD']
   openbsdish = ['OpenBSD']
@@ -29,180 +30,213 @@ describe 'snmp::client', :type => 'class' do
   context 'on a supported osfamily, default parameters' do
     redhatish.each do |os|
       describe "for osfamily RedHat, operatingsystem #{os}" do
-        let(:params) {{}}
-        let :facts do {
-          :osfamily               => 'RedHat',
-          :operatingsystem        => os,
-          :operatingsystemrelease => '6.4',
-          :lsbmajdistrelease      => '6',
-          :operatingsystemmajrelease => '6'
-        }
+        let(:params) { {} }
+        let :facts do
+          {
+            osfamily: 'RedHat',
+            operatingsystem: os,
+            operatingsystemrelease: '6.4',
+            lsbmajdistrelease: '6',
+            operatingsystemmajrelease: '6'
+          }
         end
-        it { should contain_package('snmp-client').with(
-          :ensure => 'present',
-          :name   => 'net-snmp-utils'
-        )}
-        it { should contain_file('snmp.conf').with(
-          :ensure  => 'present',
-          :mode    => '0644',
-          :owner   => 'root',
-          :group   => 'root',
-          :path    => '/etc/snmp/snmp.conf',
-          :require => ['Package[snmp-client]', 'File[/etc/snmp]']
-        )}
+
+        it {
+          is_expected.to contain_package('snmp-client').with(
+            ensure: 'present',
+            name: 'net-snmp-utils'
+          )
+        }
+        it {
+          is_expected.to contain_file('snmp.conf').with(
+            ensure: 'present',
+            mode: '0644',
+            owner: 'root',
+            group: 'root',
+            path: '/etc/snmp/snmp.conf',
+            require: ['Package[snmp-client]', 'File[/etc/snmp]']
+          )
+        }
       end
     end
 
     debianish.each do |os|
       describe "for osfamily Debian, operatingsystem #{os}" do
-        let(:params) {{}}
-        let :facts do {
-          :osfamily        => 'Debian',
-          :operatingsystem => os,
-          :operatingsystemrelease => '6.0.7',
-          :lsbmajdistrelease      => '6',
-          :operatingsystemmajrelease => '6'
-        }
+        let(:params) { {} }
+        let :facts do
+          {
+            osfamily: 'Debian',
+            operatingsystem: os,
+            operatingsystemrelease: '6.0.7',
+            lsbmajdistrelease: '6',
+            operatingsystemmajrelease: '6'
+          }
         end
-        it { should contain_package('snmp-client').with(
-          :ensure => 'present',
-          :name   => 'snmp'
-        )}
-        it { should contain_file('snmp.conf').with(
-          :ensure  => 'present',
-          :mode    => '0644',
-          :owner   => 'root',
-          :group   => 'root',
-          :path    => '/etc/snmp/snmp.conf',
-          :require => 'Package[snmp-client]'
-        )}
+
+        it {
+          is_expected.to contain_package('snmp-client').with(
+            ensure: 'present',
+            name: 'snmp'
+          )
+        }
+        it {
+          is_expected.to contain_file('snmp.conf').with(
+            ensure: 'present',
+            mode: '0644',
+            owner: 'root',
+            group: 'root',
+            path: '/etc/snmp/snmp.conf',
+            require: 'Package[snmp-client]'
+          )
+        }
       end
     end
 
     suseish.each do |os|
       describe "for osfamily Suse, operatingsystem #{os}" do
-        let(:params) {{}}
-        let :facts do {
-          :osfamily               => 'Suse',
-          :operatingsystem        => os,
-          :operatingsystemrelease => '11.1',
-          :lsbmajdistrelease      => '11',
-          :operatingsystemmajrelease => '11'
-        }
+        let(:params) { {} }
+        let :facts do
+          {
+            osfamily: 'Suse',
+            operatingsystem: os,
+            operatingsystemrelease: '11.1',
+            lsbmajdistrelease: '11',
+            operatingsystemmajrelease: '11'
+          }
         end
-        it { should_not contain_package('snmp-client') }
-        it { should contain_file('snmp.conf').with(
-          :ensure  => 'present',
-          :mode    => '0644',
-          :owner   => 'root',
-          :group   => 'root',
-          :path    => '/etc/snmp/snmp.conf',
-          :require => nil
-        )}
+
+        it { is_expected.not_to contain_package('snmp-client') }
+        it {
+          is_expected.to contain_file('snmp.conf').with(
+            ensure: 'present',
+            mode: '0644',
+            owner: 'root',
+            group: 'root',
+            path: '/etc/snmp/snmp.conf',
+            require: nil
+          )
+        }
       end
     end
 
     freebsdish.each do |os|
       describe "for osfamily FreeBSD, operatingsystem #{os}" do
-        let(:params) {{}}
-        let :facts do {
-          :osfamily               => 'FreeBSD',
-          :operatingsystem        => os,
-          :operatingsystemrelease => '9.2',
-          :operatingsystemmajrelease => '9'
-        }
+        let(:params) { {} }
+        let :facts do
+          {
+            osfamily: 'FreeBSD',
+            operatingsystem: os,
+            operatingsystemrelease: '9.2',
+            operatingsystemmajrelease: '9'
+          }
         end
-        it { should contain_package('snmp-client').with(
-          :ensure => 'present',
-          :name   => 'net-mgmt/net-snmp'
-        )}
-        it { should_not contain_file('snmp.conf').with(
-          :ensure  => 'present',
-          :mode    => '0755',
-          :owner   => 'root',
-          :group   => 'wheel',
-          :path    => '/usr/local/etc/snmp/snmp.conf',
-          :require => nil
-        )}
+
+        it {
+          is_expected.to contain_package('snmp-client').with(
+            ensure: 'present',
+            name: 'net-mgmt/net-snmp'
+          )
+        }
+        it {
+          is_expected.not_to contain_file('snmp.conf').with(
+            ensure: 'present',
+            mode: '0755',
+            owner: 'root',
+            group: 'wheel',
+            path: '/usr/local/etc/snmp/snmp.conf',
+            require: nil
+          )
+        }
       end
     end
 
     openbsdish.each do |os|
       describe "for osfamily OpenBSD, operatingsystem #{os}" do
-        let(:params) {{}}
-        let :facts do {
-          :osfamily               => 'OpenBSD',
-          :operatingsystem        => os,
-          :operatingsystemrelease => '5.9',
-          :operatingsystemmajrelease => '5'
-        }
+        let(:params) { {} }
+        let :facts do
+          {
+            osfamily: 'OpenBSD',
+            operatingsystem: os,
+            operatingsystemrelease: '5.9',
+            operatingsystemmajrelease: '5'
+          }
         end
-        it { should contain_package('snmp-client').with(
-          :ensure => 'present',
-          :name   => 'net-snmp'
-        )}
-        it { should_not contain_file('snmp.conf').with(
-          :ensure  => 'present',
-          :mode    => '0755',
-          :owner   => 'root',
-          :group   => 'wheel',
-          :path    => '/etc/snmp/snmp.conf',
-          :require => nil
-        )}
+
+        it {
+          is_expected.to contain_package('snmp-client').with(
+            ensure: 'present',
+            name: 'net-snmp'
+          )
+        }
+        it {
+          is_expected.not_to contain_file('snmp.conf').with(
+            ensure: 'present',
+            mode: '0755',
+            owner: 'root',
+            group: 'wheel',
+            path: '/etc/snmp/snmp.conf',
+            require: nil
+          )
+        }
       end
     end
   end
 
   context 'on a supported osfamily, custom parameters' do
-    let :facts do {
-      :osfamily               => 'RedHat',
-      :operatingsystem        => 'RedHat',
-      :operatingsystemrelease => '6.4',
-      :lsbmajdistrelease      => '6',
-      :operatingsystemmajrelease => '6'
-    }
+    let :facts do
+      {
+        osfamily: 'RedHat',
+        operatingsystem: 'RedHat',
+        operatingsystemrelease: '6.4',
+        lsbmajdistrelease: '6',
+        operatingsystemmajrelease: '6'
+      }
     end
 
     describe 'ensure => absent' do
-      let(:params) {{ :ensure => 'absent' }}
-      it { should contain_package('snmp-client').with_ensure('absent') }
-      it { should contain_file('snmp.conf').with_ensure('absent') }
+      let(:params) { { ensure: 'absent' } }
+
+      it { is_expected.to contain_package('snmp-client').with_ensure('absent') }
+      it { is_expected.to contain_file('snmp.conf').with_ensure('absent') }
     end
 
     describe 'ensure => badvalue' do
-      let(:params) {{ :ensure => 'badvalue' }}
-      it 'should fail' do
-        expect {
-          should raise_error(Puppet::Error, /ensure parameter must be present or absent/)
-        }
+      let(:params) { { ensure: 'badvalue' } }
+
+      it 'fails' do
+        expect do
+          is_expected.to raise_error(Puppet::Error, %r{ensure parameter must be present or absent})
+        end
       end
     end
 
     describe 'autoupgrade => true' do
-      let(:params) {{ :autoupgrade => true }}
-      it { should contain_package('snmp-client').with_ensure('latest') }
-      it { should contain_file('snmp.conf').with_ensure('present') }
+      let(:params) { { autoupgrade: true } }
+
+      it { is_expected.to contain_package('snmp-client').with_ensure('latest') }
+      it { is_expected.to contain_file('snmp.conf').with_ensure('present') }
     end
 
     describe 'autoupgrade => badvalue' do
-      let(:params) {{ :autoupgrade => 'badvalue' }}
-      it 'should fail' do
-        expect {
-          should raise_error(Puppet::Error, /"badvalue" is not a boolean./)
-        }
+      let(:params) { { autoupgrade: 'badvalue' } }
+
+      it 'fails' do
+        expect do
+          is_expected.to raise_error(Puppet::Error, %r{"badvalue" is not a boolean.})
+        end
       end
     end
 
     describe 'snmp_config => [ "defVersion 2c", "defCommunity public" ]' do
-      let(:params) {{ :snmp_config => [ 'defVersion 2c', 'defCommunity public' ] }}
-      it { should contain_file('snmp.conf') }
-      it 'should contain File[snmp.conf] with contents "defVersion 2c" and "defCommunity public"' do
+      let(:params) { { snmp_config: ['defVersion 2c', 'defCommunity public'] } }
+
+      it { is_expected.to contain_file('snmp.conf') }
+      it 'contains File[snmp.conf] with contents "defVersion 2c" and "defCommunity public"' do
         verify_contents(catalogue, 'snmp.conf', [
-          'defVersion 2c',
-          'defCommunity public',
-        ])
+                          'defVersion 2c',
+                          'defCommunity public'
+                        ])
       end
     end
   end
-
 end
--- spec/defines/snmp_snmpv3_user_spec.rb
+++ spec/defines/snmp_snmpv3_user_spec.rb
@@ -2,254 +2,286 @@
 
 require 'spec_helper'
 
-describe 'snmp::snmpv3_user', :type => 'define' do
-
+describe 'snmp::snmpv3_user', type: 'define' do
   context 'on a supported osfamily' do
-    let :facts do {
-      :osfamily               => 'RedHat',
-      :operatingsystem        => 'CentOS',
-      :operatingsystemrelease => '6.4',
-      :lsbmajdistrelease      => '6',
-      :operatingsystemmajrelease => '6'
-    }
+    let :facts do
+      {
+        osfamily: 'RedHat',
+        operatingsystem: 'CentOS',
+        operatingsystemrelease: '6.4',
+        lsbmajdistrelease: '6',
+        operatingsystemmajrelease: '6'
+      }
     end
 
     describe 'authtype => badString' do
       let(:title) { 'authtype' }
 
-      let :params do {
-        :authpass => 'myauthpass',
-        :authtype => 'badString',
-      }
+      let :params do
+        {
+          authpass: 'myauthpass',
+          authtype: 'badString'
+        }
       end
 
-      it 'should fail' do
-        expect {
-          should raise_error(Puppet::Error, /$authtype must be either SHA or MD5./)
-        }
+      it 'fails' do
+        expect do
+          is_expected.to raise_error(Puppet::Error, %r{$authtype must be either SHA or MD5.})
+        end
       end
     end
 
     describe 'privtype => badString' do
       let(:title) { 'privtype' }
 
-      let :params do {
-        :authpass => 'myauthpass',
-        :privtype => 'badString',
-      }
+      let :params do
+        {
+          authpass: 'myauthpass',
+          privtype: 'badString'
+        }
       end
 
-      it 'should fail' do
-        expect {
-          should raise_error(Puppet::Error, /$privtype must be either AES or DES./)
-        }
+      it 'fails' do
+        expect do
+          is_expected.to raise_error(Puppet::Error, %r{$privtype must be either AES or DES.})
+        end
       end
     end
 
     describe 'daemon => badString' do
       let(:title) { 'daemon' }
 
-      let :params do {
-        :authpass => 'myauthpass',
-        :daemon   => 'badString',
-      }
+      let :params do
+        {
+          authpass: 'myauthpass',
+          daemon: 'badString'
+        }
       end
 
-      it 'should fail' do
-        expect {
-          should raise_error(Puppet::Error, /$daemon must be either snmpd or snmptrapd./)
-        }
+      it 'fails' do
+        expect do
+          is_expected.to raise_error(Puppet::Error, %r{$daemon must be either snmpd or snmptrapd.})
+        end
       end
     end
   end
 
   context 'on a supported osfamily, RedHat' do
-    let :facts do {
-      :osfamily               => 'RedHat',
-      :operatingsystem        => 'CentOS',
-      :operatingsystemrelease => '6.4',
-      :lsbmajdistrelease      => '6',
-      :operatingsystemmajrelease => '6'
-    }
+    let :facts do
+      {
+        osfamily: 'RedHat',
+        operatingsystem: 'CentOS',
+        operatingsystemrelease: '6.4',
+        lsbmajdistrelease: '6',
+        operatingsystemmajrelease: '6'
+      }
     end
 
     describe 'with default settings' do
       let(:title) { 'myDEFAULTuser' }
 
-      let :params do {
-        :authpass => 'myauthpass',
-      }
+      let :params do
+        {
+          authpass: 'myauthpass'
+        }
       end
 
-      it { should contain_exec('create-snmpv3-user-myDEFAULTuser').with(
-        :command => 'service snmpd stop ; sleep 5 ; echo "createUser myDEFAULTuser SHA \"myauthpass\"" >>/var/lib/net-snmp/snmpd.conf && touch /var/lib/net-snmp/myDEFAULTuser-snmpd',
-        :creates => '/var/lib/net-snmp/myDEFAULTuser-snmpd',
-        :require => [ 'Package[snmpd]', 'File[var-net-snmp]' ],
-        :before  => 'Service[snmpd]'
-      )}
+      it {
+        is_expected.to contain_exec('create-snmpv3-user-myDEFAULTuser').with(
+          command: 'service snmpd stop ; sleep 5 ; echo "createUser myDEFAULTuser SHA \"myauthpass\"" >>/var/lib/net-snmp/snmpd.conf && touch /var/lib/net-snmp/myDEFAULTuser-snmpd',
+          creates: '/var/lib/net-snmp/myDEFAULTuser-snmpd',
+          require: ['Package[snmpd]', 'File[var-net-snmp]'],
+          before: 'Service[snmpd]'
+        )
+      }
     end
 
     describe 'with all settings' do
       let(:title) { 'myALLuser' }
 
-      let :params do {
-        :authpass => 'myauthpass',
-        :authtype => 'MD5',
-        :privpass => 'myprivpass',
-        :privtype => 'DES'
-      }
+      let :params do
+        {
+          authpass: 'myauthpass',
+          authtype: 'MD5',
+          privpass: 'myprivpass',
+          privtype: 'DES'
+        }
       end
 
-      it { should contain_exec('create-snmpv3-user-myALLuser').with(
-        :command => 'service snmpd stop ; sleep 5 ; echo "createUser myALLuser MD5 \"myauthpass\" DES \"myprivpass\"" >>/var/lib/net-snmp/snmpd.conf && touch /var/lib/net-snmp/myALLuser-snmpd',
-        :creates => '/var/lib/net-snmp/myALLuser-snmpd',
-        :require => [ 'Package[snmpd]', 'File[var-net-snmp]' ],
-        :before  => 'Service[snmpd]'
-      )}
+      it {
+        is_expected.to contain_exec('create-snmpv3-user-myALLuser').with(
+          command: 'service snmpd stop ; sleep 5 ; echo "createUser myALLuser MD5 \"myauthpass\" DES \"myprivpass\"" >>/var/lib/net-snmp/snmpd.conf && touch /var/lib/net-snmp/myALLuser-snmpd',
+          creates: '/var/lib/net-snmp/myALLuser-snmpd',
+          require: ['Package[snmpd]', 'File[var-net-snmp]'],
+          before: 'Service[snmpd]'
+        )
+      }
     end
 
     describe 'with snmptrapd settings' do
       let(:title) { 'myTRAPuser' }
 
-      let :params do {
-        :authpass => 'myauthpass',
-        :daemon   => 'snmptrapd',
-      }
+      let :params do
+        {
+          authpass: 'myauthpass',
+          daemon: 'snmptrapd'
+        }
       end
 
-      it { should contain_exec('create-snmpv3-user-myTRAPuser').with(
-        :command => 'service snmptrapd stop ; sleep 5 ; echo "createUser myTRAPuser SHA \"myauthpass\"" >>/var/lib/net-snmp/snmptrapd.conf && touch /var/lib/net-snmp/myTRAPuser-snmptrapd',
-        :creates => '/var/lib/net-snmp/myTRAPuser-snmptrapd',
-        :require => [ 'Package[snmpd]', 'File[var-net-snmp]' ],
-        :before  => 'Service[snmptrapd]'
-      )}
+      it {
+        is_expected.to contain_exec('create-snmpv3-user-myTRAPuser').with(
+          command: 'service snmptrapd stop ; sleep 5 ; echo "createUser myTRAPuser SHA \"myauthpass\"" >>/var/lib/net-snmp/snmptrapd.conf && touch /var/lib/net-snmp/myTRAPuser-snmptrapd',
+          creates: '/var/lib/net-snmp/myTRAPuser-snmptrapd',
+          require: ['Package[snmpd]', 'File[var-net-snmp]'],
+          before: 'Service[snmptrapd]'
+        )
+      }
     end
   end
 
   context 'on a supported osfamily, Debian' do
-    let :facts do {
-      :osfamily               => 'Debian',
-      :operatingsystem        => 'Ubuntu',
-      :operatingsystemrelease => '12.04',
-      :lsbmajdistrelease      => '12',
-      :operatingsystemmajrelease => '12'
-    }
+    let :facts do
+      {
+        osfamily: 'Debian',
+        operatingsystem: 'Ubuntu',
+        operatingsystemrelease: '12.04',
+        lsbmajdistrelease: '12',
+        operatingsystemmajrelease: '12'
+      }
     end
 
     describe 'with default settings' do
       let(:title) { 'myDEFAULTuser' }
 
-      let :params do {
-        :authpass => 'myauthpass',
-      }
+      let :params do
+        {
+          authpass: 'myauthpass'
+        }
       end
 
-      it { should contain_exec('create-snmpv3-user-myDEFAULTuser').with(
-        :command => 'service snmpd stop ; sleep 5 ; echo "createUser myDEFAULTuser SHA \"myauthpass\"" >>/var/lib/snmp/snmpd.conf && touch /var/lib/snmp/myDEFAULTuser-snmpd',
-        :creates => '/var/lib/snmp/myDEFAULTuser-snmpd',
-        :require => [ 'Package[snmpd]', 'File[var-net-snmp]' ],
-        :before  => 'Service[snmpd]'
-      )}
+      it {
+        is_expected.to contain_exec('create-snmpv3-user-myDEFAULTuser').with(
+          command: 'service snmpd stop ; sleep 5 ; echo "createUser myDEFAULTuser SHA \"myauthpass\"" >>/var/lib/snmp/snmpd.conf && touch /var/lib/snmp/myDEFAULTuser-snmpd',
+          creates: '/var/lib/snmp/myDEFAULTuser-snmpd',
+          require: ['Package[snmpd]', 'File[var-net-snmp]'],
+          before: 'Service[snmpd]'
+        )
+      }
     end
 
     describe 'with all settings' do
       let(:title) { 'myALLuser' }
 
-      let :params do {
-        :authpass => 'myauthpass',
-        :authtype => 'MD5',
-        :privpass => 'myprivpass',
-        :privtype => 'DES'
-      }
+      let :params do
+        {
+          authpass: 'myauthpass',
+          authtype: 'MD5',
+          privpass: 'myprivpass',
+          privtype: 'DES'
+        }
       end
 
-      it { should contain_exec('create-snmpv3-user-myALLuser').with(
-        :command => 'service snmpd stop ; sleep 5 ; echo "createUser myALLuser MD5 \"myauthpass\" DES \"myprivpass\"" >>/var/lib/snmp/snmpd.conf && touch /var/lib/snmp/myALLuser-snmpd',
-        :creates => '/var/lib/snmp/myALLuser-snmpd',
-        :require => [ 'Package[snmpd]', 'File[var-net-snmp]' ],
-        :before  => 'Service[snmpd]'
-      )}
+      it {
+        is_expected.to contain_exec('create-snmpv3-user-myALLuser').with(
+          command: 'service snmpd stop ; sleep 5 ; echo "createUser myALLuser MD5 \"myauthpass\" DES \"myprivpass\"" >>/var/lib/snmp/snmpd.conf && touch /var/lib/snmp/myALLuser-snmpd',
+          creates: '/var/lib/snmp/myALLuser-snmpd',
+          require: ['Package[snmpd]', 'File[var-net-snmp]'],
+          before: 'Service[snmpd]'
+        )
+      }
     end
 
     describe 'with snmptrapd settings' do
       let(:title) { 'myTRAPuser' }
 
-      let :params do {
-        :authpass => 'myauthpass',
-        :daemon   => 'snmptrapd',
-      }
+      let :params do
+        {
+          authpass: 'myauthpass',
+          daemon: 'snmptrapd'
+        }
       end
 
-      it { should contain_exec('create-snmpv3-user-myTRAPuser').with(
-        :command => 'service snmpd stop ; sleep 5 ; echo "createUser myTRAPuser SHA \"myauthpass\"" >>/var/lib/snmp/snmptrapd.conf && touch /var/lib/snmp/myTRAPuser-snmptrapd',
-        :creates => '/var/lib/snmp/myTRAPuser-snmptrapd',
-        :require => [ 'Package[snmpd]', 'File[var-net-snmp]' ],
-        :before  => 'Service[snmpd]'
-      )}
+      it {
+        is_expected.to contain_exec('create-snmpv3-user-myTRAPuser').with(
+          command: 'service snmpd stop ; sleep 5 ; echo "createUser myTRAPuser SHA \"myauthpass\"" >>/var/lib/snmp/snmptrapd.conf && touch /var/lib/snmp/myTRAPuser-snmptrapd',
+          creates: '/var/lib/snmp/myTRAPuser-snmptrapd',
+          require: ['Package[snmpd]', 'File[var-net-snmp]'],
+          before: 'Service[snmpd]'
+        )
+      }
     end
   end
 
   context 'on a supported osfamily, Suse' do
-    let :facts do {
-      :osfamily               => 'Suse',
-      :operatingsystem        => 'SLES',
-      :operatingsystemrelease => '11.1',
-      :lsbmajdistrelease      => '11',
-      :operatingsystemmajrelease => '11'
-    }
+    let :facts do
+      {
+        osfamily: 'Suse',
+        operatingsystem: 'SLES',
+        operatingsystemrelease: '11.1',
+        lsbmajdistrelease: '11',
+        operatingsystemmajrelease: '11'
+      }
     end
 
     describe 'with default settings' do
       let(:title) { 'myDEFAULTuser' }
 
-      let :params do {
-        :authpass => 'myauthpass',
-      }
+      let :params do
+        {
+          authpass: 'myauthpass'
+        }
       end
 
-      it { should contain_exec('create-snmpv3-user-myDEFAULTuser').with(
-        :command => 'service snmpd stop ; sleep 5 ; echo "createUser myDEFAULTuser SHA \"myauthpass\"" >>/var/lib/net-snmp/snmpd.conf && touch /var/lib/net-snmp/myDEFAULTuser-snmpd',
-        :creates => '/var/lib/net-snmp/myDEFAULTuser-snmpd',
-        :require => [ 'Package[snmpd]', 'File[var-net-snmp]' ],
-        :before  => 'Service[snmpd]'
-      )}
+      it {
+        is_expected.to contain_exec('create-snmpv3-user-myDEFAULTuser').with(
+          command: 'service snmpd stop ; sleep 5 ; echo "createUser myDEFAULTuser SHA \"myauthpass\"" >>/var/lib/net-snmp/snmpd.conf && touch /var/lib/net-snmp/myDEFAULTuser-snmpd',
+          creates: '/var/lib/net-snmp/myDEFAULTuser-snmpd',
+          require: ['Package[snmpd]', 'File[var-net-snmp]'],
+          before: 'Service[snmpd]'
+        )
+      }
     end
 
     describe 'with all settings' do
       let(:title) { 'myALLuser' }
 
-      let :params do {
-        :authpass => 'myauthpass',
-        :authtype => 'MD5',
-        :privpass => 'myprivpass',
-        :privtype => 'DES'
-      }
+      let :params do
+        {
+          authpass: 'myauthpass',
+          authtype: 'MD5',
+          privpass: 'myprivpass',
+          privtype: 'DES'
+        }
       end
 
-      it { should contain_exec('create-snmpv3-user-myALLuser').with(
-        :command => 'service snmpd stop ; sleep 5 ; echo "createUser myALLuser MD5 \"myauthpass\" DES \"myprivpass\"" >>/var/lib/net-snmp/snmpd.conf && touch /var/lib/net-snmp/myALLuser-snmpd',
-        :creates => '/var/lib/net-snmp/myALLuser-snmpd',
-        :require => [ 'Package[snmpd]', 'File[var-net-snmp]' ],
-        :before  => 'Service[snmpd]'
-      )}
+      it {
+        is_expected.to contain_exec('create-snmpv3-user-myALLuser').with(
+          command: 'service snmpd stop ; sleep 5 ; echo "createUser myALLuser MD5 \"myauthpass\" DES \"myprivpass\"" >>/var/lib/net-snmp/snmpd.conf && touch /var/lib/net-snmp/myALLuser-snmpd',
+          creates: '/var/lib/net-snmp/myALLuser-snmpd',
+          require: ['Package[snmpd]', 'File[var-net-snmp]'],
+          before: 'Service[snmpd]'
+        )
+      }
     end
 
     describe 'with snmptrapd settings' do
       let(:title) { 'myTRAPuser' }
 
-      let :params do {
-        :authpass => 'myauthpass',
-        :daemon   => 'snmptrapd',
-      }
+      let :params do
+        {
+          authpass: 'myauthpass',
+          daemon: 'snmptrapd'
+        }
       end
 
-      it { should contain_exec('create-snmpv3-user-myTRAPuser').with(
-        :command => 'service snmptrapd stop ; sleep 5 ; echo "createUser myTRAPuser SHA \"myauthpass\"" >>/var/lib/net-snmp/snmptrapd.conf && touch /var/lib/net-snmp/myTRAPuser-snmptrapd',
-        :creates => '/var/lib/net-snmp/myTRAPuser-snmptrapd',
-        :require => [ 'Package[snmpd]', 'File[var-net-snmp]' ],
-        :before  => 'Service[snmptrapd]'
-      )}
+      it {
+        is_expected.to contain_exec('create-snmpv3-user-myTRAPuser').with(
+          command: 'service snmptrapd stop ; sleep 5 ; echo "createUser myTRAPuser SHA \"myauthpass\"" >>/var/lib/net-snmp/snmptrapd.conf && touch /var/lib/net-snmp/myTRAPuser-snmptrapd',
+          creates: '/var/lib/net-snmp/myTRAPuser-snmptrapd',
+          require: ['Package[snmpd]', 'File[var-net-snmp]'],
+          before: 'Service[snmptrapd]'
+        )
+      }
     end
   end
-
 end
