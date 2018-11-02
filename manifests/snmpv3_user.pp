bb38374bdb303790c6e2bdcb9b26c9d60343b2c4
Stefan Möding <stm@kill-9.net>
2018-11-02 18:56:54 +0100

    Compare existing and expected passwords to trigger user creation
    This rewrite of `snmp::snmpv3_user` (re)creates the user if it does not
    exist or the stored passwords do not match the expected values. It uses the
    `snmpv3_user` fact to retrieve the currently configured SNMPv3 users and
    calculates the expected password hashes by calling the
    `snmp::snmpv3_usm_hash` function using the cleartext password.
    
    Updating the passwords in Puppet will now correctly update the passwords in
    the SNMP configuration file. (closes #4).

--- manifests/snmpv3_user.pp
+++ manifests/snmpv3_user.pp
@@ -24,38 +24,54 @@
 #   Which daemon file in which to write the user.  snmpd or snmptrapd
 #
 define snmp::snmpv3_user (
-  $authpass,
-  Enum['SHA','MD5'] $authtype = 'SHA',
-
-  $privpass = undef,
-  Enum['AES','DES'] $privtype = 'AES',
-
-  Enum['snmpd','snmptrapd'] $daemon = 'snmpd'
+  String[8]                 $authpass,
+  Enum['SHA','MD5']         $authtype = 'SHA',
+  Optional[String[8]]       $privpass = undef,
+  Enum['AES','DES']         $privtype = 'AES',
+  Enum['snmpd','snmptrapd'] $daemon   = 'snmpd'
 ) {
   include snmp
 
   if ($daemon == 'snmptrapd') and ($facts['os']['family'] != 'Debian') {
     $service_name   = 'snmptrapd'
-    $service_before = Service['snmptrapd']
   } else {
     $service_name   = 'snmpd'
-    $service_before = Service['snmpd']
   }
 
-  if $privpass {
-    $cmd = "createUser ${title} ${authtype} \\\"${authpass}\\\" ${privtype} \\\"${privpass}\\\""
-  } else {
-    $cmd = "createUser ${title} ${authtype} \\\"${authpass}\\\""
+  $cmd = $privpass ? {
+    undef   => "createUser ${title} ${authtype} \\\"${authpass}\\\"",
+    default => "createUser ${title} ${authtype} \\\"${authpass}\\\" ${privtype} \\\"${privpass}\\\""
   }
-  exec { "create-snmpv3-user-${title}":
-    path    => '/bin:/sbin:/usr/bin:/usr/sbin',
-    # TODO: Add "rwuser ${title}" (or rouser) to /etc/snmp/${daemon}.conf
-    command => "service ${service_name} stop ; sleep 5 ; \
-echo \"${cmd}\" >>${snmp::params::var_net_snmp}/${daemon}.conf \
-&& touch ${snmp::params::var_net_snmp}/${title}-${daemon}",
-    creates => "${snmp::params::var_net_snmp}/${title}-${daemon}",
-    user    => 'root',
-    require => [ Package['snmpd'], File['var-net-snmp'], ],
-    before  => $service_before,
+
+  if ($title in $facts['snmpv3_user']) {
+    # user details from config are available as fact
+    $usm_user = $facts['snmpv3_user'][$title]
+
+    $authhash = snmp::snmpv3_usm_hash($authtype, $usm_user['engine'], $authpass)
+
+    # privacy protocol key may be empty; truncate to 128 bits if used
+    $privhash = empty($privpass) ? {
+      true    => '',
+      default => snmp::snmpv3_usm_hash($authtype, $usm_user['engine'], $privpass, 128)
+    }
+
+    # (re)create the user if at least one of the hashes is different
+    $create = ($authhash != $usm_user['authhash']) or ($privhash != $usm_user['privhash'])
+  }
+  else {
+    # user is unknown
+    $create = true
+  }
+
+  if $create {
+    exec { "create-snmpv3-user-${title}":
+      path    => '/bin:/sbin:/usr/bin:/usr/sbin',
+      # TODO: Add "rwuser ${title}" (or rouser) to /etc/snmp/${daemon}.conf
+      command => "service ${service_name} stop ; sleep 5 ; \
+echo \"${cmd}\" >>${snmp::params::var_net_snmp}/${daemon}.conf",
+      user    => 'root',
+      require => [ Package['snmpd'], File['var-net-snmp'], ],
+      before  => Service[$service_name],
+    }
   }
 }
